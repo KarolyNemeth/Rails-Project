8c423b74cf90e6cd2ffa644e68e631ce82a71d22
Karoly1996 <futyi1996@hotmail.com>
2024-09-14 13:40:47 -0400

    added sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+
+  delete "logout", to: "sessions#destroy"
+
   get "/about_us", to: "about#index", as: :about
 end
