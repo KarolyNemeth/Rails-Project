ed207699a58c184f0071dd5bb56b4a50c932336e
Karoly1996 <futyi1996@hotmail.com>
2024-09-27 12:52:31 -0400

    fixed update password error not rendering

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,12 +2,13 @@ class PasswordsController < ApplicationController
   before_action :require_user_logged_in!
 
   def edit
-    
   end
 
   def update
     if Current.user.update(password_params)
-      redirect_to root_path, notice: "Password updated"
+      redirect_to root_path, notice: "Password Updated"
+    else
+      render :edit
     end
   end
 
